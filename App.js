4ed963ae3089f538115454cbbf52e9d5724cde71
Iqromax Dev <iqromax@example.com>
2026-06-26 19:32:45 +0500

    Preload assets on app startup to avoid image popping delays and update package-lock

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Asset } from 'expo-asset';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import StepOneScreen from './screens/StepOneScreen';
@@ -16,6 +17,64 @@ import { useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_7
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  
+  const [assetsLoaded, setAssetsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadAssets() {
+      try {
+        await Asset.loadAsync([
+          require('./assets/avatar_alex.jpg'),
+          require('./assets/avatar_david.jpg'),
+          require('./assets/avatar_emma.jpg'),
+          require('./assets/avatar_kevin.png'),
+          require('./assets/avatar_lily.jpg'),
+          require('./assets/avatar_maks.png'),
+          require('./assets/avatar_maya.jpg'),
+          require('./assets/avatar_sophia.png'),
+          require('./assets/character_bg.png'),
+          require('./assets/info_card_bg.png'),
+          require('./assets/space_bg.jpg'),
+          require('./assets/yangi_1.png'),
+          require('./assets/yangi_2.png'),
+          require('./assets/yangi_3.png'),
+          require('./assets/yangi_4.png'),
+          require('./assets/yangi_5.png'),
+          require('./assets/yangi_6.png'),
+          require('./assets/yangi_7.png'),
+          require('./assets/yangi_8.png'),
+          require('./assets/yangi_9.png'),
+          require('./assets/yangi_10.png'),
+          require('./assets/yutuq_1.png'),
+          require('./assets/yutuq_2.png'),
+          require('./assets/yutuq_3.png'),
+          require('./assets/yutuq_4.png'),
+          require('./assets/yutuq_5.png'),
+          require('./assets/yutuq_6.png'),
+          require('./assets/yutuq_7.png'),
+          require('./assets/yutuq_8.png'),
+          require('./assets/yutuq_9.png'),
+          require('./assets/yutuq_10.png'),
+          require('./assets/yutuq_11.png'),
+          require('./assets/yutuq_12.png'),
+          require('./assets/lightning_energy.png'),
+          require('./assets/s_coin.png'),
+          require('./assets/gold_frame.png'),
+          require('./assets/gold_star.png'),
+          require('./assets/stat_icon_1.png'),
+          require('./assets/stat_icon_2.png'),
+          require('./assets/stat_icon_3.png'),
+          require('./assets/stat_icon_4.png')
+        ]);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAssetsLoaded(true);
+      }
+    }
+    loadAssets();
+  }, []);
+
   let [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium,
@@ -24,7 +83,7 @@ export default function App() {
     Inter_900Black,
   });
 
-  if (!fontsLoaded) {
+  if (!fontsLoaded || !assetsLoaded) {
     return null;
   }
 
