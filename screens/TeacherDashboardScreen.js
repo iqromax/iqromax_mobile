b3236a69a93ba0eed1343c7747125c2a0f8f49e5
Iqromax Dev <iqromax@example.com>
2026-08-22 01:33:48 +0500

    Restore calculateUserRank import in TeacherDashboardScreen.js

--- screens/TeacherDashboardScreen.js
+++ screens/TeacherDashboardScreen.js
@@ -10,6 +10,7 @@ import { Feather, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 import io from 'socket.io-client';
 import { API_URL } from '../src/config/api';
 import { LinearGradient } from 'expo-linear-gradient';
+import { calculateUserRank } from '../src/utils/rankUtils';
 import { DASHBOARD_TRANSLATIONS } from './StudentDashboardScreen';
 
 const SOCKET_SERVER_URL = API_URL.replace(/\/api\/?$/, '');
