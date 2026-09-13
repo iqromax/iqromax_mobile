fb80bec022be7662260003f263e8157fb28ba946
Iqromax Dev <iqromax@example.com>
2026-09-13 22:55:38 +0500

    Shift 2nd place podium avatar further left to 1.5%

--- screens/StudentDashboardScreen.js
+++ screens/StudentDashboardScreen.js
@@ -6195,7 +6195,7 @@ const styles = StyleSheet.create({
   podiumSecond: {
     position: 'absolute',
     top: '34%', 
-    left: '4.5%',
+    left: '1.5%',
     width: 50,
     height: 50,
     borderRadius: 25,
