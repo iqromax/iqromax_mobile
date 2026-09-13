9b0021038276344e9f6366a7c0b67e20885b31f7
Iqromax Dev <iqromax@example.com>
2026-09-13 23:01:30 +0500

    Use Platform.OS conditional left offset for Android 2nd place podium avatar

--- screens/StudentDashboardScreen.js
+++ screens/StudentDashboardScreen.js
@@ -2849,7 +2849,7 @@ export default function StudentDashboardScreen({ navigation, route }) {
         <ImageBackground source={require('../assets/ranking_podium.png')} style={styles.podiumImage} contentFit="contain">
           
           {/* 2nd Place (Left) */}
-          <View style={[styles.podiumSecond, { left: '-3%' }]}>
+          <View style={[styles.podiumSecond, Platform.OS === 'android' ? { left: -10 } : { left: '-3%' }]}>
               <Image source={top2.avatar} style={styles.podiumAvatar} />
           </View>
 
