0f6f836bd053856a3ac4430382801d3dbd4651c8
Iqromax Dev <iqromax@example.com>
2026-09-13 21:48:44 +0500

    Set camera orbit 90deg so all 3D models face directly front to user

--- components/Character3DViewer.js
+++ components/Character3DViewer.js
@@ -142,7 +142,7 @@ export function Character3DViewer({ characterIndex = 0, accessoryPath = null, he
           interaction-prompt="none"
           environment-image="neutral"
           bounds="tight"
-          camera-orbit="-90deg 75deg auto"
+          camera-orbit="90deg 75deg auto"
           min-camera-orbit="-infinity 75deg auto"
           max-camera-orbit="infinity 75deg auto"
         >
