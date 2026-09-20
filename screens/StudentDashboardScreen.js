9bb398789f2ccb6bfea065f579cd1023ab9d9e8c
Iqromax Dev <iqromax@example.com>
2026-09-21 02:12:36 +0500

    fix(ui): increase top padding for friends modal on iOS

--- screens/StudentDashboardScreen.js
+++ screens/StudentDashboardScreen.js
@@ -5582,7 +5582,7 @@ export default function StudentDashboardScreen({ navigation, route }) {
         <View style={{ flex: 1, backgroundColor: '#05050C' }}>
           <SafeAreaView style={{ flex: 1 }}>
       {/* Header */}
-      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 20, paddingTop: Platform.OS === 'ios' ? 50 : 10, paddingBottom: 20 }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 20, paddingTop: Platform.OS === 'ios' ? 65 : 10, paddingBottom: 20 }}>
         <TouchableOpacity onPress={() => setIsFriendsModalOpen(false)}>
                 <MaterialCommunityIcons name="close" size={28} color="#FFF" />
               </TouchableOpacity>
